6543f96950c533a1db4ee7ca305b108e7c23bc37
Gkood <Zb86443148>
2023-04-25 09:20:44 +0800

    IDC

--- src/plugins/table/defaults.js
+++
@@ -1,8 +0,0 @@
-import VXETable from 'vxe-table'
-
-/**
- * 修改默认配置
- */
-VXETable.setup({
-  version: 0
-})
--- src/utils/cookies.ts
+++
@@ -1,7 +0,0 @@
-import Cookies from 'js-cookie'
-
-const languageKey = 'language'
-export const getLanguage = () => Cookies.get(languageKey)
-export const setLanguage = (language:string) => Cookies.set(languageKey, language)
-
-
